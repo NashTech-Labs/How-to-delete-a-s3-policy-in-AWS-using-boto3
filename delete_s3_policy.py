725586b3f98c687b69f38b66f7bb3a799cc106ca
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 09:38:42 +0530

    Update in code

--- delete_s3_policy.py
+++ delete_s3_policy.py
@@ -1,12 +1,13 @@
+# By MuZakkir Saifi
 # import the boto3 which will use to interact  with the aws
 
 import boto3
 
-AWS_REGION = input("Enter the AWS_REGION Name")
+REGION = input("Enter the REGION Name")
 S3_BUCKET_NAME = input("Enter the bucket name")
 
-s3_client = boto3.client("s3", region_name=AWS_REGION)
+client_for_s3 = boto3.client("s3", region_name=REGION)
 
-s3_client.delete_bucket_policy(Bucket=S3_BUCKET_NAME)
+client_for_s3.delete_bucket_policy(Bucket=S3_BUCKET_NAME)
 
 print('Wow, Your bucket Policy has been deleted')
